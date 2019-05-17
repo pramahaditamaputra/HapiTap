c734277661ca280a35cc658e8620e2d9822686cd
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2019-05-17 09:16:11 +0700

    Initial Commit

---
+++ HapiTap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapiTap
+//
+//  Created by Pramahadi Tama Putra on 17/05/19.
+//  Copyright © 2019 Pramahadi Tama Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-HapiTap/Pods-HapiTap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HapiTap : NSObject
+@end
+@implementation PodsDummy_Pods_HapiTap
+@end
