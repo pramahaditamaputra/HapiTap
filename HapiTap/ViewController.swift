0393cb3b244ccd3a5b27fcca022e6154e1ea6223
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2019-05-18 13:36:37 +0700

    HapiTap Version 2.0 - Perubahan Konsep App

--- HapiTap/ViewController.swift
+++ HapiTap/ViewController.swift
@@ -12,13 +12,16 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var mainToneContainers: AnimationView!
     @IBOutlet var toneContainers: [AnimationView]!
     
     @IBOutlet weak var tapHereContainer: AnimationView!
     
     var player: AVAudioPlayer?
-    
+
     var happyBirthdayNoteCounter: Int = 0
+    var tapColorCounter: Int = 0
+    var tapSparkleCounter: Int = 0
     
     var happyBirthdayNotes: [Int] = [
         4,4,5,4,0,6,
@@ -42,7 +45,8 @@ class ViewController: UIViewController {
         "kuning",
         "oren",
         "pink",
-        "ungu"
+        "ungu",
+        "sparkleHappy"
     ]
     
     var toneSounds: [String] = [
@@ -56,18 +60,49 @@ class ViewController: UIViewController {
         "doo"
     ]
     
+    var tapToneSounds: [String] = [
+        "bubbles",
+        "clay",
+        "confetti",
+        "corona",
+        "dotted-spiral",
+        "flash-1",
+        "flash-2",
+        "flash-3",
+        "glimmer",
+        "moon",
+        "pinwheel",
+        "piston-1",
+        "piston-2",
+        "piston-3",
+        "prism-1",
+        "prism-2",
+        "prism-3",
+        "splits",
+        "squiggle",
+        "strike",
+        "suspension",
+        "timer",
+        "ufo",
+        "veil",
+        "wipe",
+        "zig-zag"
+    ]
+    
     func playSound() {
         
-        if happyBirthdayNoteCounter > 25    {
-            
+//        if happyBirthdayNoteCounter > 25    {
+        
             //Reset Song
-            happyBirthdayNoteCounter = 0
+//            happyBirthdayNoteCounter = 0
             
-        }else{
+//        }else{
             
-            print(happyBirthdayNoteCounter)
-            
-            guard let url = Bundle.main.url(forResource: toneSounds[happyBirthdayNotes[happyBirthdayNoteCounter]], withExtension: "wav", subdirectory: "ToneSound") else { return }
+//            print(happyBirthdayNoteCounter)
+        
+        var randomTapToneSoundNumber: Int = Int.random(in: 0...25)
+        
+            guard let url = Bundle.main.url(forResource: tapToneSounds[randomTapToneSoundNumber], withExtension: "mp3", subdirectory: "TapToneSound") else { return }
         
             do {
                 try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
@@ -83,12 +118,12 @@ class ViewController: UIViewController {
                 
                 player.play()
                 
-                happyBirthdayNoteCounter += 1
+//                happyBirthdayNoteCounter += 1
                 
             } catch let error {
                 print(error.localizedDescription)
             }
-        }
+//        }
     }
     
     
@@ -105,9 +140,30 @@ class ViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        tapColorCounter += 1
+        var randomScale: Int = Int.random(in: 1...10)
+        var randomMainTapToneContainer: Int = Int.random(in: 0..<6)
         
-        var randomToneBackgroundColor = Int.random(in: 0..<6)
-        self.view.backgroundColor = toneBackGroundColor[randomToneBackgroundColor]
+        if let touch = touches.first {
+            let position = touch.location(in: view)
+            UIView.animate(withDuration: 0.1) {
+                if self.tapColorCounter < 10 {
+                    self.mainToneContainers.animation = Animation.named(self.toneColors[randomMainTapToneContainer], subdirectory: "ToneColor")
+                }else{
+                    self.tapSparkleCounter += 1
+                    self.mainToneContainers.animation = Animation.named(self.toneColors[6], subdirectory: "ToneColor")
+                    
+                    if self.tapSparkleCounter > 10 {
+                        self.tapColorCounter = 0
+                        self.tapSparkleCounter = 0
+                    }
+                }
+                self.mainToneContainers.center = CGPoint(x: position.x, y: position.y)
+                self.mainToneContainers.transform = CGAffineTransform(scaleX: CGFloat(randomScale), y: CGFloat(randomScale))
+                self.mainToneContainers.play()
+            }
+        }
+        
         playSound()
         setAnimation()
     }
@@ -117,12 +173,24 @@ class ViewController: UIViewController {
         
         tapHereContainer.isHidden = true
         
-        for toneContainer in 0...9 {
+//        for toneContainer in 0...1 {
+            var randomToneBackgroundColor = Int.random(in: 0..<6)
             var randomToneContainer = Int.random(in: 0..<32)
             var randomToneColor = Int.random(in: 0..<6)
-            toneContainers[randomToneContainer].animation = Animation.named(toneColors[randomToneColor], subdirectory: "ToneColor")
-            toneContainers[randomToneContainer].play()
-        }
+            var randomSizeX = Int.random(in: 1...3)
+            var randomSizeY = Int.random(in: 1...3)
+
+            UIView.animate(withDuration: 0.1, animations: {
+//                self.view.backgroundColor = self.toneBackGroundColor[randomToneBackgroundColor]
+                if self.tapColorCounter < 10 {
+                    self.toneContainers[randomToneContainer].animation = Animation.named(self.toneColors[randomToneColor], subdirectory: "ToneColor")
+                }else{
+                    self.toneContainers[randomToneContainer].animation = Animation.named(self.toneColors[6], subdirectory: "ToneColor")
+                }
+                self.toneContainers[randomToneContainer].transform = CGAffineTransform(scaleX: CGFloat(randomSizeX), y: CGFloat(randomSizeX))
+                self.toneContainers[randomToneContainer].play()
+            })
+//        }
         
     }
     
